04b5e5c51a8635498749f03e936a2c895540265e
Dennis T Kaplan <gruessle@gmail.com>
2013-07-18 19:58:12 +0300

    Made better doc

--- sbs.go
+++ sbs.go
@@ -1,3 +1,5 @@
+// Copyright © 2013 Dennis T Kaplan <http://www.robotamer.com>
+
 // sbs can encode a struct to a byte slice and back
 // This is useful if you need to save data in a key value database such as
 // a leveldb, because a leveldb only takes bytes as values.
@@ -8,6 +10,7 @@ import (
 	"encoding/gob"
 )
 
+// Enc takes a populated struct and returns a []byte
 func Enc(o interface{}) (b []byte, err error) {
 	var buf bytes.Buffer
 	enc := gob.NewEncoder(&buf)
@@ -19,6 +22,8 @@ func Enc(o interface{}) (b []byte, err error) {
 	return
 }
 
+// Dec takes an empty struct as well as the []byte returned by Enc(),
+// and returns a populated struct.
 func Dec(o interface{}, b []byte) (interface{}, error) {
 	var buf bytes.Buffer
 	_, err := buf.Write(b)
