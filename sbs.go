3c900121a575f6ec2b6b628cd38e72c9ace6f4f5
Dennis T Kaplan <gruessle@gmail.com>
2022-03-09 03:50:22 -0800

    sbs to gob & Marshal Unmarshal
    moved sbs to gob and implemented Marshal Unmarshal

--- sbs.go
+++ sbs.go
@@ -1,34 +1,11 @@
-// sbs can encode a struct to a byte slice and back
-// This is useful if you need to save data in a key value database such as
-// a leveldb, because a leveldb only takes bytes as values.
-// You can use sbs also as the backend to satify the interfaces
-// BinaryMarshaler and BinaryUnmarshaler by simply creating two functions for
-// your struct called MarshalBinary() and UnmarshalBinary
-//
-// Example:
-// type MyStruct struct {
-//     A, B, C string
-// }
-//
-// func (o *MyStruct) MarshalBinary() (data []byte, err error) {
-//     data, err := sbs.Enc(o)
-//     return
-// }
-//
-// func (o *MyStruct) UnmarshalBinary(data []byte) error {
-//     return sbs.Dec(o, data)
-// }
-package sbs
-
-// Copyright © 2013 Dennis T Kaplan <http://www.robotamer.com>
-// The standard MIT License can be found at robotamer.com
+//Package gob implements Marshal and Unmarshal using the standard GOB package
 import (
 	"bytes"
 	"encoding/gob"
 )
 
-// Enc takes a populated struct and returns a []byte
-func Enc(o interface{}) (b []byte, err error) {
+// Marshal takes a populated struct and returns a []byte in GOB format
+func Marshal(o interface{}) (b []byte, err error) {
 	var buf bytes.Buffer
 	enc := gob.NewEncoder(&buf)
 	err = enc.Encode(o)
@@ -39,9 +16,9 @@ func Enc(o interface{}) (b []byte, err error) {
 	return
 }
 
-// Dec takes an empty struct as well as the []byte returned by Enc(),
+// Unmarshal takes an empty struct as well as the []byte returned by Marshal(),
 // and populates the given empty struct from the []byte.
-func Dec(o interface{}, b []byte) (err error) {
+func Unmarshal(b []byte, o interface{}) (err error) {
 	var buf bytes.Buffer
 	_, err = buf.Write(b)
 	if err != nil {
